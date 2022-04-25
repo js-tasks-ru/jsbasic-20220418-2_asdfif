b8f10bf30de1e6e14f01637405c4ea614cf59c66
Alexandr Burov <a.burov@sftpro.ru>
2022-04-25 21:37:38 +0300

    Resolving 3.1 - 3.5

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
-  // ваш код...
+  const [firstSubStr, ...other] = str.split('-')
+  const camelSubStr = other.map(([first, ...substr]) => {
+    return (first.toUpperCase() + substr.join(''))
+  }).join('')
+
+  return firstSubStr + camelSubStr
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,5 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str.split(' ').filter(number => Number(number)).map(number => Number(number))
+
+  return { min: Math.min(...numbers), max: Math.max(...numbers) }
 }
