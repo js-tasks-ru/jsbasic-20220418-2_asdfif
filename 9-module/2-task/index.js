4f64f123ff69a6ad32b4de99387bc7e31c327acb
Alexandr Burov <a.burov@sftpro.ru>
2022-05-28 12:56:08 +0300

    Resolving 9.1 - 9.2

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -16,6 +16,62 @@ export default class Main {
   }
 
   async render() {
-    // ... ваш код
+    this.cartIcon = new CartIcon()
+    document.body.querySelector('[data-cart-icon-holder]').append(this.cartIcon.elem)
+
+    this.carousel = new Carousel(slides);
+    document.body.querySelector('[data-carousel-holder]').append(this.carousel.elem)
+
+    this.ribbonMenu = new RibbonMenu(categories);
+    document.body.querySelector('[data-ribbon-holder]').append(this.ribbonMenu.elem)
+
+    this.stepSlider = new StepSlider({
+      steps: 5
+    })
+    document.body.querySelector('[data-slider-holder]').append(this.stepSlider.elem)
+
+    this.products = await fetch('products.json')
+      .then((response) => { return response.json() })
+      .catch(() => { return [] })
+
+    this.productsGrid = await new ProductsGrid(this.products)
+    document.body.querySelector('[data-products-grid-holder]').append(this.productsGrid.elem)
+
+    this.productsGrid.updateFilter({
+      noNuts: document.getElementById('nuts-checkbox').checked,
+      vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
+      maxSpiciness: this.stepSlider.value,
+      category: this.ribbonMenu.value
+    })
+
+    this.cart = await new Cart(this.cartIcon);
+
+    document.body.addEventListener('product-add', function(event) {
+      const product = this.products.find(product => { return product.id === event.detail })
+      this.cart.addProduct(product)
+    }.bind(this))
+
+    this.stepSlider.elem.addEventListener('slider-change', function(event) {
+      this.productsGrid.updateFilter({ maxSpiciness: event.detail })
+    }.bind(this))
+
+    this.ribbonMenu.elem.addEventListener('ribbon-select', function(event) {
+      console.log(event)
+      this.productsGrid.updateFilter({ category: event.detail })
+    }.bind(this))
+
+    document.body.querySelector('.filters').addEventListener('change', function(event) {
+      if (event.target.id === 'nuts-checkbox') {
+        this.productsGrid.updateFilter({
+          noNuts: event.target.checked
+        })
+      }
+
+      if (event.target.id === 'vegeterian-checkbox') {
+        this.productsGrid.updateFilter({
+          vegeterianOnly: event.target.checked
+        })
+      }
+    }.bind(this))
   }
 }
